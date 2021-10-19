82870e329bf446e9c23adb296c683dfe6a7546cc
Jacob Kloepper <kloepperwjacob@gmail.com>
2021-10-19 00:12:15 -0700

    initial commit

---
+++ inc/logger.h
@@ -0,0 +1,10 @@
+//#include "common.h"
+// debug on win10 host:
+typedef unsigned int uid;
+//
+
+void set_str_nak(char* cstr);
+int check_str_nak(char* cstr);
+char* map_uid_to_name(uid UID);
+
+void update_log(uid UID);
