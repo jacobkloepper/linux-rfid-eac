fc494cb51fdbae7655a5d244f902f2017ab96e87
jacobkloepper <kloepperwjacob@gmail.com>
2021-11-15 16:35:14 -0800

    fixed id getter

--- tool/google/get-ids.py
+++ tool/google/get-ids.py
@@ -11,7 +11,17 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-# Get/read auth
+def get_file_dict() -> dict:
+    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    file_dict = {}
+
+    # fill dict with all files found
+    for file_ptr in file_list:
+        file_dict[file_ptr['title']] = file_ptr['id']
+
+    return file_dict
+
+# check/update creds
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile("creds.txt")
 if gauth.credentials is None:
@@ -22,29 +32,24 @@ else:
     gauth.Authorize()
 gauth.SaveCredentialsFile("creds.txt")
 
-# get ids
-expected_names = ["reports", "logs", "data"]
-
+# open handler
 drive = GoogleDrive(gauth)
-file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 
-file_dict = {}
 
-# fill dict with all files found
-for file_ptr in file_list:
-    file_dict[file_ptr['title']] = file_ptr['id']
+# get ids
+file_dict = get_file_dict()
 
 # check found files against expected files; if expected but not found, create and upload.
+expected_names = ["reports", "logs", "data"]
 for name in expected_names:
     if (name not in file_dict):
         drive.CreateFile({'title': name, 'mimeType': 'application/vnd.google-apps.folder'}).Upload()
 
-# now that all files are in, update dict
-for file_ptr in file_list:
-    file_dict[file_ptr['title']] = file_ptr['id']
+# now that all expecteds are uploaded, get the file dict again
+file_dict = get_file_dict()
 
 # have all necessary IDs, create script to hold them
 with open("../remote/gdrive_ids.py", "w") as fh:
     fh.write(f"REPORT_FOLDER_ID = \"{file_dict['reports']}\"\n")
-    fh.write(f"LOG_FOLDER_ID= \"{file_dict['logs']}\"\n")
+    fh.write(f"LOG_FOLDER_ID = \"{file_dict['logs']}\"\n")
     fh.write(f"USER_FOLDER_ID = \"{file_dict['data']}\"\n")
